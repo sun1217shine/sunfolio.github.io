555722f0024ebc49cbcb6672037dba4d43a850c8
sun1217shine <51977778+sun1217shine@users.noreply.github.com>
2026-08-06 16:32:36 +0900

    2026ver-0806
    #intro
         - title : finish
         - list : finish
    #profile
         - intro :  in progress
         - skill :  in progress
         - career :  in progress
         - key : not yet

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
+// 정의
+const mediaQuery = window.matchMedia('(max-width: 1024px)');
+
+
+// root --number 숫자 설정
 function applyCssVariables() {
   const root = document.documentElement;
-  const mediaQuery = window.matchMedia('(max-width: 1024px)');
-
   function setVariables(e) {
     const multiplier = e.matches ? 2 : 4;
     for (let i = 1; i <= 50; i++) {
       root.style.setProperty(`--number-${i}`, `${i * multiplier}px`);
     }
   }
-
-  // 초기 실행
   setVariables(mediaQuery);
-
-  // 1024px 경계를 제어할 때만 이벤트 감지 (성능 향상)
   mediaQuery.addEventListener('change', setVariables);
 }
-
 applyCssVariables();
